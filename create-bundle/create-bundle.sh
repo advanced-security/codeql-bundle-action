92fecb15a2a7502ca4c20a87bf2b98c13e543dbd
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2023-07-25 17:33:40 -0700

    Add initial version create-bundle action

---
+++ create-bundle/create-bundle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+output_path=${RUNNER_TEMP}/codeql-bundle.tar.gz
+opts=()
+opts+=("--bundle-path" "${BUNDLE_PATH}")
+opts+=("--output" "${RUNNER_TEMP}/codeql-bundle.tar.gz")
+opts+=("--workspace" "${WORKSPACE}")
+if [[ -n ${PLATFORM} ]]; then
+    for platform in $(echo $PLATFORM | tr ',' ' '); do
+    opts+=("--platform" "${platform}")
+    done
+    # When building multiple bundles, the output path is the directory containing the bundles
+    output_path=${RUNNER_TEMP}/bundles
+    mkdir -p ${output_path}
+fi
+if [[ -n ${DEFAULT_CODE_SCANNING_CONFIG} ]]; then
+    opts+=("--code-scanning-config" "${DEFAULT_CODE_SCANNING_CONFIG}")
+fi
+
+codeql-bundle ${opts[@]} $(echo ${PACKS} | tr ',' ' ')
+
+echo "output-path=${output_path}" >> $GITHUB_OUTPUT

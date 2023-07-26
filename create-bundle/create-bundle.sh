b7d6fdd76437dc494dfd83ae61d599c47abdd614
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2023-07-25 19:41:42 -0700

    Address incorrect parameters

--- create-bundle/create-bundle.sh
+++ create-bundle/create-bundle.sh
@@ -4,17 +4,17 @@ set -e
 
 output_path=${RUNNER_TEMP}/codeql-bundle.tar.gz
 opts=()
-opts+=("--bundle-path" "${BUNDLE_PATH}")
-opts+=("--output" "${RUNNER_TEMP}/codeql-bundle.tar.gz")
+opts+=("--bundle" "${BUNDLE_PATH}")
 opts+=("--workspace" "${WORKSPACE}")
-if [[ -n ${PLATFORM} ]]; then
-    for platform in $(echo $PLATFORM | tr ',' ' '); do
+if [[ -n ${PLATFORMS} ]]; then
+    for platform in $(echo $PLATFORMS | tr ',' ' '); do
     opts+=("--platform" "${platform}")
     done
     # When building multiple bundles, the output path is the directory containing the bundles
     output_path=${RUNNER_TEMP}/bundles
     mkdir -p ${output_path}
 fi
+opts+=("--output" "${output_path}")
 if [[ -n ${DEFAULT_CODE_SCANNING_CONFIG} ]]; then
     opts+=("--code-scanning-config" "${DEFAULT_CODE_SCANNING_CONFIG}")
 fi
